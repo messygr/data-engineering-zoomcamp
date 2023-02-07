02858b57c86326b167dcce6e36269dcc2d6772d0
messygr <46906387+messygr@users.noreply.github.com>
2023-02-07 16:24:51 +0100

    Update etl_web_to_gcs_pm_git.py

--- week_2/etl_web_to_gcs_pm_git.py
+++ week_2/etl_web_to_gcs_pm_git.py
@@ -2,15 +2,10 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-from random import randint
-
 
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
-    # if randint(0, 1) > 0:
-    #     raise Exception
-
     df = pd.read_parquet(dataset_url)
     return df
 
