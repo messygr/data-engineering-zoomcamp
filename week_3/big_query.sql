4dd059e0a2ba201f7ead5c69e575102dad11feb4
messygr <46906387+messygr@users.noreply.github.com>
2023-02-10 13:56:42 +0100

    Update big_query.sql

--- week_3/big_query.sql
+++ week_3/big_query.sql
@@ -1,1 +1,42 @@
+CREATE OR REPLACE EXTERNAL TABLE `datazc-pm.taxi_trips.fhv_trips_2019`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://dtc_data_lake_datazc-pm/data/fhv_tripdata_2019-*.csv.gz']
+);
 
+select 
+count(*) 
+from `datazc-pm.taxi_trips.fhv_trips_2019`
+;
+
+CREATE OR REPLACE TABLE datazc-pm.taxi_trips.fhv_trips_2019_non_partitoned AS
+SELECT * FROM datazc-pm.taxi_trips.fhv_trips_2019
+;
+
+select
+count(distinct affiliated_base_number)
+from datazc-pm.taxi_trips.fhv_trips_2019
+;
+
+select
+count(distinct affiliated_base_number)
+from datazc-pm.taxi_trips.fhv_trips_2019_non_partitoned
+;
+
+select
+count(*)
+from datazc-pm.taxi_trips.fhv_trips_2019_non_partitoned
+where PUlocationID is null
+  and DOlocationID is null
+
+
+CREATE OR REPLACE TABLE datazc-pm.taxi_trips.fhv_trips_2019_partitoned_clustered
+PARTITION BY DATE(pickup_datetime)
+CLUSTER BY affiliated_base_number AS
+SELECT * FROM datazc-pm.taxi_trips.fhv_trips_2019
+;
+
+select
+distinct affiliated_base_number
+from datazc-pm.taxi_trips.fhv_trips_2019_non_partitoned
+where pickup_datetime between '2019-03-01' and '2019-04-01'
